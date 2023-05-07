380402b460386cefc687f6e24145950138a28351
Marzena Tatara <marzenatatara77@gmail.com>
2023-05-07 09:15:00 +0200

    remove sth

--- js/script.js
+++ js/script.js
@@ -4,7 +4,6 @@ let button = document.querySelector(".button");
 let header = document.querySelector(".header");
 let changeBackground = document.querySelector(".changeBackground");
 let mainContener = document.querySelector(".mainContener");
-let footer = document.querySelector(".footer");
 
 
 button.addEventListener("click", () => {
@@ -23,24 +22,7 @@ changeBackground.addEventListener("click", () => {
     }
 });
 
-let number1 = 11;
-let number2 = 10;
 
-let message = number1 > number2 ? "number1 jest większe" : "number1 nie jest większe";
-
-console.log(message);
-
-let powrot = document.querySelector(".powrot");
-button = document.querySelector(".button");
-
-button.addEventListener("click", () => {
-
-    if (powrot.innerText === "Wróć na górę") {
-        powrot.innerText = "Idź dalej";
-    } else if (powrot.innerText === "Idź dalej") {
-        powrot.innerText = "Wróć na górę";
-    }
-});
 
 
 
